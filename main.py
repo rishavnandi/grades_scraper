41f895fb64952d341b87194375c7763fbe602109
Rishav Nandi <rishavnandi2@gmail.com>
2023-07-20 18:23:46 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,54 @@
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+
+service = Service(executable_path="D:\projects\grades_scraper\chromedriver.exe")
+
+driver = webdriver.Chrome(service=service)
+
+# Navigate to the result website
+driver.get("http://49.249.101.34/CGUResults/search-result.php?hasv=Qi5UZWNoKjQqUmVndWxhcioyMDIxLTIwMjIqSnVuZSBFdmVuIFNlbSBFeGFtIDIwMjM=")
+driver.maximize_window()
+
+# Create an empty list to store the data
+data = []
+
+# Input registration numbers in the specified range
+for reg_no in range(2101020000, 2101021000):
+    try:
+        reg_no_input = driver.find_element(By.ID, "regn_no")
+        reg_no_input.clear()
+        reg_no_input.send_keys(str(reg_no))
+        reg_no_input.send_keys(Keys.ENTER)
+
+        # Scrape student name and SGPA
+        student_name = driver.find_element(By.XPATH, "//li[2]")
+        student_name = student_name.text[13:]
+        student_name = student_name.strip()
+        sgpa = driver.find_element(By.XPATH, "//span[b[contains(text(), 'SGPA:')]]")
+        sgpa = sgpa.text[5:]
+        sgpa = sgpa.strip()
+
+        # Print the student's registration number, name and SGPA
+        print(f"Registration Number: {reg_no}, Name: {student_name}, SGPA: {sgpa}")
+
+        # Append the data to the list
+        data.append({
+            'Registration Number': reg_no,
+            'Name': student_name,
+            'SGPA': sgpa
+        })
+    except:
+        # Handle the case when a student with the registration number does not exist
+        print(f"Error for registration number: {reg_no}")
+
+# Create a pandas DataFrame from the data
+df = pd.DataFrame(data)
+
+# Save the DataFrame to a CSV file
+df.to_csv('student_results.csv', index=False)
